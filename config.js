edd8bad92224f36c9e0dddc8742fbd977b159b3e
Stephen A. Wilson <Stephen-356@hotmail.com>
2017-03-01 23:03:43 -0500

    customize error messages

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+const privateConfig = require('./config.private.js');
+
 module.exports = {
 
   // Your bot name. Typically, this is your bot's username without the discriminator.
@@ -7,12 +9,13 @@ module.exports = {
   // The bot's command prefix. The bot will recognize as command any message that begins with it.
   // i.e: "-nix foo" will trigger the command "foo",
   //      whereas "Nix foo" will do nothing at all.
-  prefix:  "-nix",
+  prefix:  "!",
+  separator: '',
 
   // Your bot's user token. If you don't know what that is, go here:
   // https://discordapp.com/developers/applications/me
   // Then create a new application and grab your token.
-  token: "",
+  token: privateConfig.token,
 
   // If this option is enabled, the bot will delete the message that triggered it, and its own
   // response, after the specified amount of time has passed.
@@ -21,7 +24,7 @@ module.exports = {
   // MANAGE_MESSAGES - 	0x00002000
   // More info: https://discordapp.com/developers/docs/topics/permissions
   deleteAfterReply: {
-    enabled: true,
+    enabled: false,
     time: 10000, // In milliseconds
   }
 };
