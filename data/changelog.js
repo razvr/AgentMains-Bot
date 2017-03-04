83ed01adc5880db558f387b64430d77866e4c71c
Stephen A. Wilson <Stephen-356@hotmail.com>
2017-03-04 00:14:38 -0500

    add changelog command

---
+++ data/changelog.js
@@ -0,0 +1,24 @@
+module.exports = [
+  [
+    {
+      type: 'New command: !changelog',
+      msg: 'See new features and changes since my last update'
+    },
+    {
+      type: 'New command: !boop',
+      msg: 'Boop!'
+    },
+    {
+      type: 'New command: !info',
+      msg: 'Get information about me.'
+    },
+    {
+      type: 'Update',
+      msg: 'Added regions for PS, XBox, and Asia'
+    },
+    {
+      type: 'Update',
+      msg: 'I play some games now. Recommend me some titles!'
+    }
+  ],
+];
