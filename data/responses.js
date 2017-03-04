86e7f3bed20ed58f0cc40900027941cf51f3f575
Stephen A. Wilson <Stephen-356@hotmail.com>
2017-03-04 00:32:57 -0500

    hotfix boop message

--- data/responses.js
+++ data/responses.js
@@ -1,8 +1,8 @@
 module.exports = {
   boop: [
-    'Boop. http://i.imgur.com/Tu54A2K.gif',
-    'Boop ;) http://imgur.com/AoMC49d',
-    'Boop http://imgur.com/xIEGKka0',
-    'Boop? http://imgur.com/CDt9g9L',
+    'Boop.',
+    'Boop ;)',
+    'Boop!',
+    'Boop?',
   ],
 };
