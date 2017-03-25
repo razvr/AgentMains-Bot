484a87f8222ec1375429619ed98941ce4bdc925a
Stephen Wiggins <steve.wiggins7@gmail.com>
2017-03-25 10:50:34 -0400

    fix changelog

--- data/changelog.js
+++ data/changelog.js
@@ -1,40 +1,8 @@
 module.exports = [
   [
     {
-      type: 'Feature: reply types',
-      msg: 'I can now reply to you, say something, or send a private message',
-    }
-  ],
-  [
-    {
-      type: 'Bugfix: commands are now case-insensitive',
-      msg: '!Region and !region are now the same command'
-    }
-  ],
-  [
-    {
-      type: 'New command: !changelog',
-      msg: 'See new features and changes since my last update'
-    },
-    {
-      type: 'New command: !boop',
-      msg: 'Boop!'
-    },
-    {
       type: 'New command: !highnoon',
       msg: 'Is it high noon?'
-    },
-    {
-      type: 'New command: !info',
-      msg: 'Get information about me.'
-    },
-    {
-      type: 'Update',
-      msg: 'Added regions for PS, XBox, and Asia'
-    },
-    {
-      type: 'Update',
-      msg: 'I play some games now. Recommend me some titles!'
     }
   ],
 ];
