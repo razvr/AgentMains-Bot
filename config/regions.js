cf99f11fb66c390398e83db5979f302d073726ee
Stephen A. Wilson <Stephen-356@hotmail.com>
2017-06-09 19:07:25 -0400

    fix eslint errors

--- config/regions.js
+++ config/regions.js
@@ -6,7 +6,7 @@ module.exports = [
       'NA',
       'US',
       'Canada',
-    ]
+    ],
   },
   {
     name: 'Europe',
@@ -14,13 +14,34 @@ module.exports = [
     alias: [
       'EU',
       'UK',
-    ]
+    ],
   },
   {
     name: 'Asia',
     role: 'Asia',
     alias: [
       'Asia',
-    ]
+    ],
+  },
+  {
+    name: 'PlayStation',
+    role: 'PlayStation',
+    alias: [
+      'PS4',
+      'PS3',
+      'PS',
+      'PSN',
+    ],
+  },
+  {
+    name: 'Xbox',
+    role: 'XBox',
+    alias: [
+      'xb',
+      'xbox',
+      'xb1',
+      'xbl',
+      'xbo',
+    ],
   },
 ];
