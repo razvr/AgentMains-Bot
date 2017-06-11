d8cec82e216c3884675b6c926713b3a27af4344f
Stephen A. Wilson <Stephen-356@hotmail.com>
2017-06-11 18:25:30 -0400

    split platforms out into config file

---
+++ config/platforms.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    name: 'PlayStation',
+    role: 'PlayStation',
+    alias: [
+      'PS4',
+      'PS3',
+      'PS',
+      'PSN',
+    ],
+  },
+  {
+    name: 'Xbox',
+    role: 'XBox',
+    alias: [
+      'xb',
+      'xbox',
+      'xb1',
+      'xbl',
+      'xbo',
+    ],
+  },
+];
