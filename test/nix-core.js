ec8859cc27c8ab12c019b043d3b9a9c595899d59
Stephen A. Wilson <Stephen-356@hotmail.com>
2017-11-13 17:25:11 -0500

    use beforeEach instead of befores

--- test/nix-core.js
+++ test/nix-core.js
@@ -1,5 +1,6 @@
 const expect = require('chai').expect;
-const sinon = require('sinon');
+const sinon = require('sinon').createSandbox();
+
 const Discord = require('discord.js');
 
 const NixCore = require('./../nix-core');
@@ -7,26 +8,28 @@ const NixCore = require('./../nix-core');
 describe('NixCore', function () {
   let nix;
 
-  before(function () {
-    nix = new NixCore({});
+  beforeEach(function () {
+    nix = new NixCore({
+    });
+  });
+
+  afterEach(function () {
+    nix.shutdown();
+    sinon.restore();
   });
 
   describe('#listen()', function () {
     context('when the login token is not valid', function () {
       let expectedError;
 
-      before(function () {
+      beforeEach(function () {
         expectedError = new Error("Error from Discord.js");
-
-        Discord.Client.prototype.login =
-          sinon.stub().callsFake(() => new Promise((resolve, reject) => reject(expectedError)));
+        sinon.stub(Discord.Client.prototype, 'login').rejects(expectedError);
       });
 
       it('throws an error', function (done) {
         nix.listen().subscribe(
-          () => {
-            done(new Error('next item was passed'));
-          },
+          () => { done(new Error('next item was passed')); },
           (error) => {
             expect(error).to.not.equal(undefined);
             expect(error.message).to.equal(expectedError.message);
