8bdc37864f3c421bc61a5403b3cf62b4291e715d
Stephen A. Wilson <Stephen-356@hotmail.com>
2017-11-27 20:52:10 -0500

    add add/remove user/role methods to permissions manager

--- test/managers/permissions-manager.js
+++ test/managers/permissions-manager.js
@@ -93,16 +93,28 @@ describe('PermissionsManager', function () {
   });
 
   describe('#setPermissionsData', function () {
+    let existingData;
     let guildId;
     let data;
 
     beforeEach(function () {
+      existingData = {
+        'admin': {
+          users: ['adminUser1', 'adminUser2'],
+          roles: ['adminRole1', 'adminRole2'],
+        },
+        'mod': {
+          users: ['modUser1', 'modUser2'],
+          roles: ['modRole1', 'modRole2'],
+        },
+      };
+
       guildId = 'guildId';
       data = {test: 'data'};
     });
 
     it('sets data in the dataSource', function (done) {
-      dataManager.setGuildData.returns(Rx.Observable.just());
+      dataManager.setGuildData.withArgs(guildId, PERMISSIONS_KEYWORD, data).returns(Rx.Observable.just(existingData));
 
       permissionsManager
         .setPermissionsData(guildId, data)
@@ -114,15 +126,213 @@ describe('PermissionsManager', function () {
     });
 
     it('returns the saved data from the dataSource', function (done) {
-      dataManager.setGuildData.returns(Rx.Observable.just('savedData'));
+      dataManager.setGuildData.withArgs(guildId, PERMISSIONS_KEYWORD, data).returns(Rx.Observable.just(existingData));
 
       permissionsManager
         .setPermissionsData(guildId, data)
         .subscribe(
-          (savedData) => expect(savedData).to.eql('savedData'),
+          (savedData) => expect(savedData).to.eql(existingData),
           (err) => done(err),
           () => done()
         );
+    });
+  });
+
+  describe('#addUser', function () {
+    let user;
+    let guild;
+
+    beforeEach(function () {
+      guild = Factory.create('Guild');
+      user = Factory.create('User');
+    });
+
+    context('when there is existing data', function () {
+      let existingData;
+
+      beforeEach(function () {
+        existingData = {
+          'admin': {
+            users: ['adminUser1', 'adminUser2'],
+            roles: ['adminRole1', 'adminRole2'],
+          },
+          'mod': {
+            users: ['modUser1', 'modUser2'],
+            roles: ['modRole1', 'modRole2'],
+          },
+        };
+
+        dataManager.getGuildData.withArgs(guild.id, PERMISSIONS_KEYWORD).returns(Rx.Observable.just(existingData));
+      });
+
+      it('adds the user into the existing data', function (done) {
+        let expectedData = {
+          'admin': {
+            users: ['adminUser1', 'adminUser2', user.id],
+            roles: ['adminRole1', 'adminRole2'],
+          },
+          'mod': {
+            users: ['modUser1', 'modUser2'],
+            roles: ['modRole1', 'modRole2'],
+          },
+        };
+
+        permissionsManager.addUser(guild, 'admin', user)
+          .subscribe(
+            (savedData) => {
+              expect(dataManager.setGuildData).to.have.been.calledWith(guild.id, PERMISSIONS_KEYWORD, expectedData);
+            },
+            (err) => done(err),
+            () => done()
+          );
+      });
+    });
+  });
+
+  describe('#removeUser', function () {
+    let role;
+    let guild;
+
+    beforeEach(function () {
+      guild = Factory.create('Guild');
+      user = Factory.create('User');
+    });
+
+    context('when there is existing data', function () {
+      let existingData;
+
+      beforeEach(function () {
+        existingData = {
+          'admin': {
+            users: ['adminUser1', user.id, 'adminUser2'],
+            roles: ['adminRole1', 'adminRole2'],
+          },
+          'mod': {
+            users: ['modUser1', 'modUser2'],
+            roles: ['modRole1', 'modRole2'],
+          },
+        };
+
+        dataManager.getGuildData.withArgs(guild.id, PERMISSIONS_KEYWORD).returns(Rx.Observable.just(existingData));
+      });
+
+      it('removes the user from the data', function (done) {
+        let expectedData = {
+          'admin': {
+            users: ['adminUser1', 'adminUser2'],
+            roles: ['adminRole1', 'adminRole2'],
+          },
+          'mod': {
+            users: ['modUser1', 'modUser2'],
+            roles: ['modRole1', 'modRole2'],
+          },
+        };
+
+        permissionsManager.removeUser(guild, 'admin', user)
+          .subscribe(
+            () => expect(dataManager.setGuildData).to.have.been.calledWith(guild.id, PERMISSIONS_KEYWORD, expectedData),
+            (err) => done(err),
+            () => done()
+          );
+      });
+    });
+  });
+
+  describe('#addRole', function () {
+    let role;
+    let guild;
+
+    beforeEach(function () {
+      guild = Factory.create('Guild');
+      role = Factory.create('Role');
+    });
+
+    context('when there is existing data', function () {
+      let existingData;
+
+      beforeEach(function () {
+        existingData = {
+          'admin': {
+            users: ['adminUser1', 'adminUser2'],
+            roles: ['adminRole1', 'adminRole2'],
+          },
+          'mod': {
+            users: ['modUser1', 'modUser2'],
+            roles: ['modRole1', 'modRole2'],
+          },
+        };
+
+        dataManager.getGuildData.withArgs(guild.id, PERMISSIONS_KEYWORD).returns(Rx.Observable.just(existingData));
+      });
+
+      it('adds the role to the data', function (done) {
+        let expectedData = {
+          'admin': {
+            users: ['adminUser1', 'adminUser2'],
+            roles: ['adminRole1', 'adminRole2', role.id],
+          },
+          'mod': {
+            users: ['modUser1', 'modUser2'],
+            roles: ['modRole1', 'modRole2'],
+          },
+        };
+
+        permissionsManager.addRole(guild, 'admin', role)
+          .subscribe(
+            () => expect(dataManager.setGuildData).to.have.been.calledWith(guild.id, PERMISSIONS_KEYWORD, expectedData),
+            (err) => done(err),
+            () => done()
+          );
+      });
+    });
+  });
+
+  describe('#removeRole', function () {
+    let role;
+    let guild;
+
+    beforeEach(function () {
+      guild = Factory.create('Guild');
+      role = Factory.create('Role');
+    });
+
+    context('when there is existing data', function () {
+      let existingData;
+
+      beforeEach(function () {
+        existingData = {
+          'admin': {
+            users: ['adminUser1', 'adminUser2'],
+            roles: ['adminRole1', 'adminRole2', role.id],
+          },
+          'mod': {
+            users: ['modUser1', 'modUser2'],
+            roles: ['modRole1', 'modRole2'],
+          },
+        };
+
+        dataManager.getGuildData.withArgs(guild.id, PERMISSIONS_KEYWORD).returns(Rx.Observable.just(existingData));
+      });
+
+      it('removes the role from the data', function (done) {
+        let expectedData = {
+          'admin': {
+            users: ['adminUser1', 'adminUser2'],
+            roles: ['adminRole1', 'adminRole2'],
+          },
+          'mod': {
+            users: ['modUser1', 'modUser2'],
+            roles: ['modRole1', 'modRole2'],
+          },
+        };
+
+        permissionsManager.removeRole(guild, 'admin', role)
+          .subscribe(
+            () => expect(dataManager.setGuildData).to.have.been.calledWith(guild.id, PERMISSIONS_KEYWORD, expectedData),
+            (err) => done(err),
+            () => done()
+          );
+      });
     });
   });
 });
