1b77a621b2ac142ee1568461c0c55dca326a9ae4
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-04-05 22:49:15 -0400

    update services system

--- nix-core.js
+++ nix-core.js
@@ -14,7 +14,9 @@ const PermissionsService = require('./lib/services/permissions-service');
 const UserService = require('./lib/services/user-service');
 
 const defaultResponseStrings = require('./lib/utility/reponse-strings');
-const modules = fs.readdirSync(__dirname + '/lib/modules')
+
+// noinspection JSUnresolvedFunction
+const builtInModules = fs.readdirSync(__dirname + '/lib/modules')
   .map((file) => require(__dirname + '/lib/modules/' + file));
 
 class NixCore {
@@ -28,62 +30,134 @@ class NixCore {
     this.config.verifyConfig(); // Confirm the config is valid
 
     this.logger = NixLogger.createLogger(this.config.logger);
-
-    this._dataService = new DataService(this);
-
-    this.services = {
-      core: {
-        CommandService: new CommandService(this),
-        ConfigActionService: new ConfigActionService(this),
-        PermissionsService: new PermissionsService(this),
-        ModuleService: new ModuleService(this),
-        UserService: new UserService(this),
-      },
-    };
+    this.responseStrings = Object.assign(defaultResponseStrings, this.config.responseStrings);
+    this.services = {};
 
     this._shutdownSubject = undefined;
     this.shutdown$ = undefined;
     this.main$ = undefined;
 
-    this.responseStrings = Object.assign(defaultResponseStrings, this.config.responseStrings);
     this.streams = {};
 
-    this._discord = new Discord.Client(this.config.discord);
+    this.discord = new Discord.Client(this.config.discord);
     this._owner = null;
 
-    modules.forEach((module) => this.moduleService.addModule(module));
+    this.addService('core', DataService);
+    this.addService('core', ModuleService);
+    this.addService('core', CommandService);
+    this.addService('core', ConfigActionService);
+    this.addService('core', PermissionsService);
+    this.addService('core', UserService);
+
+    builtInModules.forEach((module) => this.addModule(module));
   }
 
-  get dataService() { return this._dataService; }
-  get commandService() { return this.getService('core', 'CommandService'); }
-  get configActionService() { return this.getService('core', 'ConfigActionService'); }
-  get permissionsService() { return this.getService('core', 'PermissionsService'); }
-  get moduleService() { return this.getService('core', 'ModuleService'); }
-  get userService() { return this.getService('core', 'UserService'); }
+  /**
+   * @deprecated
+   * @returns {DataService}
+   */
+  get dataService() {
+    this.logger.warn("Deprecation Warning: Accessing dataService directly is deprecated. Please use nix.getService('core', 'dataService') instead.");
+    return this.getService('core', 'DataService');
+  }
 
-  addService(module, Service) {
-    this.logger.debug(`adding Service: ${module}.${Service.name}`);
-    let moduleServices = this.services[module];
-    if (!moduleServices) {
-      moduleServices = {};
-      this.services[module] = moduleServices;
+  /**
+   * @deprecated
+   * @returns {CommandService}
+   */
+  get commandService() {
+    this.logger.warn("Deprecation Warning: Accessing commandService directly is deprecated. Please use nix.getService('core', 'commandService') instead.");
+    return this.getService('core', 'CommandService');
+  }
+
+  /**
+   * @deprecated
+   * @returns {ConfigActionService}
+   */
+  get configActionService() {
+    this.logger.warn("Deprecation Warning: Accessing configActionService directly is deprecated. Please use nix.getService('core', 'configActionService') instead.");
+    return this.getService('core', 'ConfigActionService');
+  }
+
+  /**
+   * @deprecated
+   * @returns {PermissionsService}
+   */
+  get permissionsService() {
+    this.logger.warn("Deprecation Warning: Accessing permissionsService directly is deprecated. Please use nix.getService('core', 'permissionsService') instead.");
+    return this.getService('core', 'PermissionsService');
+  }
+
+  /**
+   * @deprecated
+   * @returns {ModuleService}
+   */
+  get moduleService() {
+    this.logger.warn("Deprecation Warning: Accessing moduleService directly is deprecated. Please use nix.getService('core', 'moduleService') instead.");
+    return this.getService('core', 'ModuleService');
+  }
+
+  /**
+   * @deprecated
+   * @returns {UserService}
+   */
+  get userService() {
+    this.logger.warn("Deprecation Warning: Accessing userService directly is deprecated. Please use nix.getService('core', 'userService') instead.");
+    return this.getService('core', 'UserService');
+  }
+
+  /**
+   * Adds a service to Nix for use by modules, commands, and other services.
+   *
+   * @param moduleName {string} the name of the module that the server is part of
+   * @param service {Service} The service to add
+   */
+  addService(moduleName, service) {
+    if (typeof service === "function") {
+      this.logger.warn("Deprecation Warning: Passing a Class as the service is deprecated. Please pass an instance of Service");
+
+      let Service = service;
+      service = new Service(this);
+      service.name = Service.name;
     }
 
-    moduleServices[Service.name] = new Service(this);
+    let serviceKey = `${moduleName}.${service.name}`;
+
+    if (this.services[serviceKey.toLowerCase()]) {
+      let error = new Error(`The service ${serviceKey} has already been added.`);
+      error.name = "ServiceAlreadyExistsError";
+      throw error;
+    }
+
+    service._nix = this;
+
+    if (service.onInitalize) {
+      this.logger.verbose(`initializing Service: ${serviceKey}`);
+      service.onInitalize(this.config);
+    }
+
+    this.logger.verbose(`added Service: ${serviceKey}`);
+    this.services[serviceKey.toLowerCase()] = service;
   }
 
-  getService(module, serviceName) {
-    let moduleServices = this.services[module];
-    if (!moduleServices) { throw new Error('Module has no services'); }
-    return moduleServices[serviceName];
-  }
+  /**
+   * Get a Nix service
+   *
+   * @param moduleName {string} the name of the module the service is from
+   * @param serviceName {string} tjhe name of the service
+   * @returns {*} the requested service
+   */
+  getService(moduleName, serviceName) {
+    let serviceKey = `${moduleName}.${serviceName}`;
+    let service = this.services[serviceKey.toLowerCase()];
 
-  get _allServices() {
-    let list = [];
-    Object.values(this.services).forEach((moduleServices) => {
-      list = list.concat(Object.values(moduleServices));
-    });
-    return list;
+    if (!service) {
+      let error = new Error(`The service ${serviceKey} could not be found`);
+      error.name = "ServiceNotFoundError";
+      throw error;
+    }
+
+    return service;
   }
 
   /**
@@ -92,7 +166,7 @@ class NixCore {
    * @param configActions {Object} The config module to add to Nix
    */
   addConfigActions(configActions) {
-    this.configActionService.addConfigActions(configActions);
+    this.getService('core', 'configActionService').addConfigActions(configActions);
   }
 
   /**
@@ -101,15 +175,20 @@ class NixCore {
    * @param module {Object} The module to add to Nix
    */
   addModule(module) {
-    this.moduleService.addModule(module);
+    this.getService('core', 'moduleService').addModule(module);
   }
 
   /**
    * Start the discord bot
    *
-   * @return {Rx.Observable} an observable stream to subscribe to
+   * @param ready {function} (optional) A callback function for when Nix is ready
+   * @param error {function} (optional) A callback function for when an unhandled error occurs
+   * @param complete {function} (optional) A callback function for when Nix shuts down
+   * @return {Rx.Observable<string>} an observable stream to subscribe to
    */
   listen(ready, error, complete) {
+    let dataService = this.getService('core', 'dataService');
+
     if (!this.listening) {
       //use replay subjects to let future subscribers know that the event has already passed.
       this._shutdownSubject = new Rx.ReplaySubject();
@@ -129,7 +208,7 @@ class NixCore {
             this.discord
               .guilds
               .array()
-              .forEach((guild) => this.logger.debug(`In guild '${guild.name}' (${guild.id})`))
+              .forEach((guild) => this.logger.verbose(`In guild '${guild.name}' (${guild.id})`))
           )
           .flatMap(() => this.findOwner())
           .do((owner) => this.logger.info(`Found owner ${owner.tag}`))
@@ -139,10 +218,11 @@ class NixCore {
           .merge(this._startEventStreams())
           .do(() => this.logger.info(`Event streams started`))
           .do(() => this.logger.info(`Starting onNixListen hooks`))
-          .flatMap(() => this.dataService.onNixListen())
+          .flatMap(() => dataService.onNixListen()) // prepare dataService first
           .flatMap(() =>
             Rx.Observable
-              .from(this._allServices)
+              .from(Object.values(this.services))
+              .filter((service) => service.name !== 'DataService')
               .filter((service) => typeof service.onNixListen !== 'undefined')
               .flatMap((service) => service.onNixListen())
               .defaultIfEmpty(true)
@@ -153,10 +233,11 @@ class NixCore {
           .flatMap(() =>
             Rx.Observable
               .from(this.discord.guilds.array())
-              .flatMap((guild) => this.dataService.onNixJoinGuild(guild).map(guild))
+              .flatMap((guild) => dataService.onNixJoinGuild(guild).map(guild))
               .flatMap((guild) =>
                 Rx.Observable
-                  .from(this._allServices)
+                  .from(Object.values(this.services))
+                  .filter((service) => service.name !== 'DataService')
                   .filter((service) => typeof service.onNixJoinGuild !== 'undefined')
                   .flatMap((service) => service.onNixJoinGuild(guild))
               )
@@ -188,6 +269,10 @@ class NixCore {
     return this._listenSubject;
   }
 
+  /**
+   * Checks if Nix is listening to Discord
+   * @returns {boolean}
+   */
   get listening() {
     return !!this.main$;
   }
@@ -207,7 +292,7 @@ class NixCore {
    * @param message
    * @param options
    *
-   * @return {Rx.Observable} an observable stream to subscribe to
+   * @return {Rx.Observable<string>} an observable stream to subscribe to
    */
   messageOwner(message, options={}) {
     if (this.owner === null) {
@@ -215,15 +300,6 @@ class NixCore {
     }
 
     return Rx.Observable.fromPromise(this.owner.send(message, options));
-  }
-
-  /**
-   * Returns the Discord client instance that the bot is using.
-   *
-   * @return {Discord.Client}
-   */
-  get discord() {
-    return this._discord;
   }
 
   /**
@@ -235,13 +311,24 @@ class NixCore {
     return this._owner;
   }
 
+  /**
+   * Finds the owner of this bot in Discord
+   * @returns {Rx.Observable<User>}
+   */
   findOwner() {
     return Rx.Observable
-      .fromPromise(this._discord.fetchUser(this.config.ownerUserId))
+      .fromPromise(this.discord.fetchUser(this.config.ownerUserId))
       .do((user) => this._owner = user);
   }
 
+  /**
+   * Prepares the database
+   * @private
+   * @returns {Rx.Observable<any>}
+   */
   _readyDataSource() {
+    let moduleService = this.getService('core', 'moduleService');
+
     let guilds = this.discord.guilds;
     if (guilds.size === 0) {
       return Rx.Observable.return();
@@ -249,7 +336,7 @@ class NixCore {
 
     return Rx.Observable
       .from(guilds.values())
-      .flatMap((guild) => this.moduleService.prepareDefaultData(this, guild.id))
+      .flatMap((guild) => moduleService.prepareDefaultData(this, guild.id))
       .last();
   }
 
@@ -259,14 +346,17 @@ class NixCore {
    * @private
    */
   _startEventStreams() {
+    let moduleService = this.getService('core', 'moduleService');
+    let commandService = this.getService('core', 'commandService');
+
     // Create a stream for all the Discord events
     Object.values(Discord.Constants.Events).forEach((eventType) => {
       let streamName = eventType + '$';
-      this.logger.debug(`adding stream nix.streams.${streamName}`);
+      this.logger.silly(`adding stream nix.streams.${streamName}`);
       this.streams[streamName] =
         Rx.Observable
           .fromEvent(
-            this._discord,
+            this.discord,
             eventType,
             function(...args) {
               if(args.length > 1) { return args; }
@@ -277,10 +367,9 @@ class NixCore {
 
     // Create Nix specific event streams
     this.streams.command$ =
-      this.streams
-        .message$
+      this.streams.message$
         .filter((message) => message.channel.type === 'text')
-        .filter((message) => this.commandService.msgIsCommand(message));
+        .filter((message) => commandService.msgIsCommand(message));
 
     // Apply takeUntil and share to all streams
     for(let streamName in this.streams) {
@@ -291,20 +380,20 @@ class NixCore {
     }
 
     // Listen to events
-    this.streams.command$.subscribe(
-      (message) =>
-        this.commandService
-          .runCommandForMsg(message)
-          .subscribe()
+    this.streams.command$
+      .flatMap((message) => commandService.runCommandForMsg(message) )
+      .subscribe(
+        () => {},
+        (error) => { throw error; }
       );
-    this.streams.guildCreate$.subscribe((guild) =>
-      this.moduleService
-        .prepareDefaultData(this, guild.id)
-        .merge([
-          this.commandService.onNixJoinGuild(guild),
-        ])
-        .subscribe()
-    );
+
+    this.streams.guildCreate$
+      .flatMap((guild) => moduleService.prepareDefaultData(this, guild.id).map(() => guild))
+      .flatMap((guild) => commandService.onNixJoinGuild(guild))
+      .subscribe(
+        () => {},
+        (error) => { throw error; }
+      );
 
     return Rx.Observable
       .merge(Object.values(this.streams))
@@ -324,7 +413,7 @@ class NixCore {
     let content = this.responseStrings.commandRun.unhandledException.forUser({owner: context.nix.owner});
     context.message.channel.send(content);
 
-    return Rx.Observable.return();
+    return Rx.Observable.of();
   }
 
   createErrorEmbed(context, error) {
