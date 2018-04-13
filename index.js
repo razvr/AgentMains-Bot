946972c07206f45ac74a0cc7962d1c83655e5322
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-04-12 21:56:54 -0400

    export models from nix core

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 module.exports = require('./lib/nix-core');
+module.exports.Command = require('./lib/models/command');
+module.exports.ConfigAction = require('./lib/models/config-action');
+module.exports.Context = require('./lib/models/context');
+module.exports.Module = require('./lib/models/module');
 module.exports.NixConfig = require('./lib/models/nix-config');
+module.exports.Response = require('./lib/models/response');
+module.exports.Service = require('./lib/models/service');
