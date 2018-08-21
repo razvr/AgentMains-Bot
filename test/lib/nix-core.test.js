af9cda0644cf1010f99b764204bbdd224d6a16a3
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-08-21 19:06:31 -0400

    test output of the handleHook observables

--- test/lib/nix-core.test.js
+++ test/lib/nix-core.test.js
@@ -19,38 +19,62 @@ describe('Nix', function () {
         this.returnValue = undefined;
       });
 
-      it('turns the value into an Observable', function () {
+      it('turns the value into an Observable', function (done) {
         let result$ = this.nix.handleHook(this.returnValue);
+        expect(result$).to.be.an.instanceOf(Rx.Observable);
+
+        result$.subscribe(this.nextCallback, (error) => done(error), () => {
+          expect(this.nextCallback).to.have.been.calledOnceWith('');
+          done();
+        });
       });
     });
 
     context('when the hook return value an observable', function () {
       beforeEach(function () {
-        this.returnValue = Rx.Observable.of('');
+        this.returnValue = Rx.Observable.of('response');
       });
 
-      it('turns the value into an Observable', function () {
+      it('turns the value into an Observable', function (done) {
         let result$ = this.nix.handleHook(this.returnValue);
+        expect(result$).to.be.an.instanceOf(Rx.Observable);
+
+        result$.subscribe(this.nextCallback, (error) => done(error), () => {
+          expect(this.nextCallback).to.have.been.calledOnceWith('response');
+          done();
+        });
       });
     });
 
     context('when the hook return value is a promise', function () {
       beforeEach(function () {
-        this.returnValue = new Promise((resolve) => resolve(''));
+        this.returnValue = new Promise((resolve) => resolve('response'));
       });
 
-      it('turns the value into an Observable', function () {
+      it('turns the value into an Observable', function (done) {
         let result$ = this.nix.handleHook(this.returnValue);
+        expect(result$).to.be.an.instanceOf(Rx.Observable);
+
+        result$.subscribe(this.nextCallback, (error) => done(error), () => {
+          expect(this.nextCallback).to.have.been.calledOnceWith('response');
+          done();
+        });
       });
     });
 
     context('when the hook return value is an object', function () {
       beforeEach(function () {
-        this.returnValue = {returned: true};
+        this.returnValue = { returned: true };
       });
 
-      it('turns the value into an Observable', function () {
+      it('turns the value into an Observable', function (done) {
         let result$ = this.nix.handleHook(this.returnValue);
+        expect(result$).to.be.an.instanceOf(Rx.Observable);
+
+        result$.subscribe(this.nextCallback, (error) => done(error), () => {
+          expect(this.nextCallback).to.have.been.calledOnceWith(this.returnValue);
+          done();
+        });
       });
     });
   });
