f084ce3bbfd660e1bf42c39e664663fc39cce96e
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-08-21 21:36:27 -0400

    add debug flag to mock logger

--- test/support/mock-logger.js
+++ test/support/mock-logger.js
@@ -1,9 +1,14 @@
 const NixLogger = require("../../lib/utility/nix-logger");
 
 class MockNixLogger {
-  constructor() {
+  constructor(logMessages=false) {
     Object.keys(NixLogger.logLevels.levels).forEach((logLevel) => {
-      this[logLevel] = () => {};
+      this[logLevel] = (message) => {
+        if(logMessages) {
+          // eslint-disable-next-line no-console
+          console.log(`${logLevel}: ${message}`);
+        }
+      };
     });
   }
 }
