da82820ef60e76e38d1cfd4037ee2591de4e40ea
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-08-22 22:02:27 -0400

    replace fakes with stubs in MockClient

--- test/support/discord/mock-client.js
+++ test/support/discord/mock-client.js
@@ -1,11 +1,11 @@
 class MockClient {
   constructor() {
-    this.login = sinon.fake.resolves(true);
+    this.login = () => new Promise((resolve) => resolve(true));
 
-    this.fetchUser = sinon.fake.resolves({
+    this.fetchUser = () => new Promise((resolve) => resolve({
       tag: 'mock_user',
       send: sinon.fake.resolves(''),
-    });
+    }));
 
     this.guilds = {
       size: 0,
@@ -14,10 +14,10 @@ class MockClient {
       map: sinon.fake.returns([]),
     };
 
-    this.addEventListener = sinon.fake();
-    this.removeEventListener = sinon.fake();
+    this.addEventListener = () => {};
+    this.removeEventListener = () => {};
 
-    this.destroy = sinon.fake.resolves(true);
+    this.destroy = () => new Promise((resolve) => resolve(true));
   }
 }
 
