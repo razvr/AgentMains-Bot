021b1c169bb2bf7a027899d032accd993a621c3e
Stephen A. Wilson <Stephen-356@hotmail.com>
2019-03-31 12:24:42 -0400

    export createNixStub

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ module.exports.Module = require('./lib/models/module');
 module.exports.Service = require('./lib/models/service');
 module.exports.Command = require('./lib/models/command');
 module.exports.ConfigAction = require('./lib/models/config-action');
+
+module.exports.test = {
+  createNixStub: require('./test/support/create-nix-stub'),
+};
