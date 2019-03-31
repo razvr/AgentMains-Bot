3b4046ff42725370b9fed511b26a6fbd17c1d9e2
Stephen A. Wilson <Stephen-356@hotmail.com>
2019-03-31 00:15:20 -0400

    update observable-matches to catch errors

--- test/observable-matchers.js
+++ test/observable-matchers.js
@@ -3,62 +3,83 @@ let Rx = require('rx');
 module.exports = function (chai) {
   var Assertion = chai.Assertion;
 
-  Assertion.addProperty('observable', function () {
-    new Assertion(this._obj).to.be.an.instanceOf(Rx.Observable);
-  });
-
-  Assertion.addMethod('emitLength', function (length) {
-    new Assertion(this._obj).to.be.observable;
-    this._obj.count(() => true).subscribe((emittedLength) => new Assertion(emittedLength).to.eq(length));
-  });
-
-  Assertion.addMethod('emit', function (items) {
-    new Assertion(this._obj).to.be.observable;
-    this._obj.toArray().subscribe((emittedItems) => new Assertion(emittedItems).to.deep.equal(items));
-  });
+  Assertion.addProperty('observable', toBeObservable);
+  Assertion.addMethod('emitLength', toEmitLength);
+  Assertion.addMethod('emit', toEmit);
+  Assertion.addMethod('close', toClose);
+  Assertion.addMethod('complete', toClose);
 
   Assertion.overwriteMethod('throw', function (_super) {
     return function (errorLike, errMatcher) {
       if (this._obj instanceof Rx.Observable) {
-        this._obj
-          .subscribe(
-            () => {},
-            (error) => {
-              new Assertion(error).to.be.an.instanceOf(errorLike);
-
-              if (errMatcher) {
-                if (typeof errMatcher === "string") {
-                  new Assertion(error.message).to.eq(errMatcher);
-                } else {
-                  Object.entries(errMatcher).forEach(([property, expected]) => {
-                    new Assertion(error[property]).to.eq(expected);
-                  });
-                }
-              }
-            },
-            () => { throw new Error("Expected stream to throw an error."); });
+        return toThrow.apply(this, arguments);
       } else {
         return _super.apply(this, arguments);
       }
     };
   });
 
-  Assertion.addMethod('close', function (done, callback) {
+  function toBeObservable() {
     new Assertion(this._obj).to.be.an.instanceOf(Rx.Observable);
+  }
 
-    let onClose = () => {
-      if (callback) { callback(); }
-      done();
-    };
+  function toEmitLength(length) {
+    new Assertion(this._obj).to.be.observable;
+    this._obj = Rx.Observable.merge(
+      this._obj
+        .count(() => true)
+        .map((emittedLength) => new Assertion(emittedLength).to.eq(length))
+        .ignoreElements(),
+      this._obj,
+    );
+  }
 
-    this._obj.subscribe(() => {}, onClose, onClose);
-  });
+  function toEmit(items) {
+    new Assertion(this._obj).to.be.observable;
+    this._obj = Rx.Observable.merge(
+      this._obj
+        .toArray()
+        .map((emittedItems) => new Assertion(emittedItems).to.deep.equal(items))
+        .ignoreElements(),
+      this._obj,
+    );
+  }
 
-  Assertion.addMethod('complete', function (done, callback) {
-    new Assertion(this._obj).to.be.an.instanceOf(Rx.Observable);
-    this._obj.subscribe(() => {}, (error) => done(error), () => {
-      if (callback) { callback(); }
-      done();
-    });
-  });
+  function toThrow(errorLike, errMatcher) {
+    new Assertion(this._obj).to.be.observable;
+
+    this._obj = this._obj
+      .catch((error) => {
+        new Assertion(error).to.be.an.instanceOf(errorLike);
+
+        if (errMatcher) {
+          if (typeof errMatcher === "string") {
+            new Assertion(error.message).to.eq(errMatcher);
+          } else {
+            Object.entries(errMatcher).forEach(([property, expected]) => {
+              new Assertion(error[property]).to.eq(expected);
+            });
+          }
+        }
+
+        caught = true;
+        return Rx.Observable.empty();
+      })
+      .toArray()
+      .do(() => {
+        if (!caught) {
+          throw new Error('expected to throw an error, but none was thrown.');
+        }
+      });
+
+    let caught = false;
+  }
+
+  function toClose(done) {
+    this._obj.subscribe(
+      () => {},
+      (error) => done(error),
+      () => done(),
+    );
+  }
 };
