385ae46237dc26cbf170242f5f65a16ec9ed654b
Stephen A. Wilson <Stephen-356@hotmail.com>
2019-05-12 14:16:41 -0400

    install chai-subset

--- test/test-helper.js
+++ test/test-helper.js
@@ -1,8 +1,10 @@
 const chai = require("chai");
 const sinon = require("sinon");
 const sinonChai = require("sinon-chai");
+const chaiSubset = require("chai-subset");
 
 chai.use(sinonChai);
+chai.use(chaiSubset);
 
 global.sinon = sinon;
 global.expect = chai.expect;
